532347bb923bdb0ca5d64b243aafbf683f2562f5
Alexis Davidson <alexisdavidson00@gmail.com>
2022-07-24 11:46:13 +0200

    Update camera parent

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
@@ -9,11 +10,62 @@ public class GameController : MonoBehaviour
     [SerializeField] TMP_Text text;
     [SerializeField] string testData = "";
     List<int> finishers = new List<int>();
+    bool raceStarted;
+
+    float updateCameraParentDelay = 0.2f;
+    float updateCameraParentLast;
+
+    struct curve
+    {
+        public float a;
+        public float b;
+        public float xToReach10;
+    }
+    List<curve> curves;
+
+    void FixedUpdate()
+    {
+        if (raceStarted)
+        {
+            UpdateCameraParent();
+        }
+    }
+
+    void UpdateCameraParent()
+    {
+        if (Time.time - updateCameraParentLast > updateCameraParentDelay)
+        {
+            var mostAheadHorse = playerHorses.OrderByDescending(item => item.transform.position.x).FirstOrDefault();
+            Camera.main.transform.parent = mostAheadHorse?.transform;
+
+            updateCameraParentLast = Time.time;
+        }
+    }
 
     void StartGame(string data)
     {
         ParseData(data);
+        ComputeCurves();
         ActivateHorses();
+        raceStarted = true;
+    }
+
+    void ComputeCurves()
+    {
+        // f(x) = x + a sin(b x)
+        // f(x) = 10
+        // 10 - x = a sin(bx)
+        // -x = a sin(bx) - 10
+        // x/a = sin(bx) - 10/a
+        // x/a - sin(bx) = -10/a
+        curves = new List<curve>();
+        foreach(var finisher in finishers)
+        {
+            var c = new curve();
+            c.a = Random.Range(0.0f, 1);
+            c.b = Random.Range(0.0f, 1);
+            curves.Add(c);
+        }
     }
 
     void ActivateHorses()
